1efeb525b4aacbe1c4a4625f0a8dd94326c36b79
Tomohide Ishigami <gogo5nta@gmail.com>
2020-08-23 20:37:10 +0900

    Add files via upload

---
+++ 03_ubuntu_memo/200823_ubuntu1804_nvidia_cuda_docker/200823_00_tool_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# rename English holder
+LANG=C xdg-user-dirs-gtk-update
+
+# install chromium-browser
+sudo apt-get install chromium-browser
+
+# install terminator
+sudo apt-get install terminator
+
---
+++ 03_ubuntu_memo/200823_ubuntu1804_nvidia_cuda_docker/200823_01_nividia_driver_cuda.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# --------------------------------------------------------
+# Ubuntu18.04にNVIDIA Container Toolkitをインストールする
+# https://qiita.com/Wisteria30/items/6f1e35e600e93ff2c54b
+# --------------------------------------------------------
+# HP通りやる。ただしcuda-10-1と指定
+# URL：https://qiita.com/Wisteria30/items/6f1e35e600e93ff2c54b
+# URL: https://medium.com/@exesse/cuda-10-1-installation-on-ubuntu-18-04-lts-d04f89287130
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update
+# 自動で合うdriverを入れてくれる
+# sudo ubuntu-drivers autoinstallだと435, 以下コマンドだと418のインストールを確認
+sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda-10-1
+
+# 以下を.bashrcに追加
+# export PATH="/usr/local/cuda/bin:$PATH"
+# export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+
+echo "# CUDA setting" >> ~/.bashrc
+echo 'export PATH="/usr/local/cuda/bin:$PATH"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
